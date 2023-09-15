65da12e996a24d293ddb24146c561c473dc8b31a
anassaahi <anasfasisahi6@gmail.com>
2023-09-15 17:39:06 +0500

    message

---
+++ task05.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	system("color 24");
+	cout <<	"  ####   ##       ##  ####     ##### "<<endl;
+	cout <<	"##   ##  ## #     ## ##  ##  ##     #"<<endl;
+	cout <<	"#######  ##  #    ## ######   ##     "<<endl;
+	cout <<	"##   ##  ##   #   ## ##  ##     ###                            "<<endl;
+	cout <<	"##   ##  ##    #  ## ##  ##        ##                          "<<endl;
+	cout <<	"##   ##  ##   	 # ## ##  ##	   ##				"<<endl;
+	cout << "##   ##  ##      ### ##  ##  ######          			"<<endl;
+	} 
